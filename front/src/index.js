53c16767ba1de9e092faeb3a7df22a64a4782f1a
Emma Montoya <109782695+Emma22Montoya@users.noreply.github.com>
2022-12-04 09:10:02 -0600

    Update index.js

--- front/src/index.js
+++ front/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+Axios.defaults.baseURL="https://santo-capricho-uicui.herokuapp.com/"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
