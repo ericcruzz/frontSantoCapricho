cc57d88bb5ec597c94a45f95cd4102b53efd0953
ericcruzz <119624039+ericcruzz@users.noreply.github.com>
2022-12-04 15:27:36 -0600

    Update ComandaPlatilloCRUD.js

--- front/src/ComandaPlatillo/ComandaPlatilloCRUD.js
+++ front/src/ComandaPlatillo/ComandaPlatilloCRUD.js
@@ -50,7 +50,7 @@ showModal=(client)=>{
 
 newModal= () =>{
     this.setState({modalInsert : true, modalType : true})
-    this.newSumbmit();
+    this.newSumbmitHandler();
 }
 
 changeHandler = (e) =>{
